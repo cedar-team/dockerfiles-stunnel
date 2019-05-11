18a79673ab259d3f13ee177aeaff2a2292471953
Taylor Hakes <taylor@cedar.com>
2019-05-11 17:26:04 -0400

    Changed key expiration to 10 years

--- stunnel.sh
+++ stunnel.sh
@@ -24,7 +24,7 @@ if [[ ! -f ${STUNNEL_KEY} ]]; then
         exit 1
     fi
 
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ${STUNNEL_KEY} -out ${STUNNEL_CRT} \
+    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ${STUNNEL_KEY} -out ${STUNNEL_CRT} \
         -config /srv/stunnel/openssl.cnf 
 fi
 
